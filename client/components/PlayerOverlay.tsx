a9a2a4c25ea60eec0f85eef8ec05b7332d4ab1ad
Matthew Foley <matthewfoley333@gmail.com>
2025-09-30 23:45:25 +1300

    feat: add the player overlay

---
+++ client/components/PlayerOverlay.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react'
+
+interface WelcomeOverlayProps {
+  onClose: () => void
+}
+
+function PlayerOverlay({ onClose }: WelcomeOverlayProps) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+    return () => {
+      document.body.style.overflow = 'unset'
+    }
+  }, [])
+
+  const handleContinue = () => {
+    onClose()
+  }
+
+  const handleKeyDown = () => {
+    onClose()
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [])
+
+  return (
+    <div className="player-overlay" onClick={handleContinue}>
+      <div className="spotlight"></div>
+      <div className="player-speech-bubble">
+        <p>This is your character❗❗</p>
+        <p>
+          Control them with your arrow keys (running into the buildings takes
+          you to the projects)
+        </p>
+        <p>Click anywhere or press any key to continue...</p>
+      </div>
+    </div>
+  )
+}
+
+export default PlayerOverlay
