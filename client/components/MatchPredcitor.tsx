98fc69414677ae60b42c3547085e79bb093e0031
Matthew Foley <matthewfoley333@gmail.com>
2025-10-01 16:53:43 +1300

    adjust zone detection lines

--- client/components/MatchPredcitor.tsx
+++ client/components/MatchPredcitor.tsx
@@ -19,9 +19,12 @@ function MatchPredictor() {
       style={{ position: 'absolute', left: 938, top: 363 }}
     >
       <div className="label-content">
-        <div className="label-title">Match Predictor ⚽</div>
+        <div className="label-title">FurtherForward 🧢</div>
+
         <div className="label-description">
-          AI-powered Premier League predictions (Coming Soon)
+          {/* AI-powered Premier League predictions (Coming Soon) */}
+          Draft website for solo business that I created before the Dev Academy
+          bootcamp
         </div>
       </div>
       <div className="label-arrow">↓</div>
