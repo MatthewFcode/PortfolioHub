fa9b4f52e2c9a02cb4fb7236f8042e364099ed45
Matthew Foley <matthewfoley333@gmail.com>
2025-10-02 12:28:00 +1300

    add borders back in

--- client/components/FilmFeels.tsx
+++ client/components/FilmFeels.tsx
@@ -1,26 +1,3 @@
-// function FilmFeels() {
-//   return (
-//     <div>
-//       <h1>FilmFeels API 🎬</h1>
-//       <p>
-//         FilmFeels is a REST API that allows for users to group movie data by
-//         mood tags. There is a built in POST feature on the front end which
-//         allows users to enter a film and the mood they were in and send this
-//         data to our database. This data is automatically enriched by our backend
-//         adding lots of data about the users entry and then making it availabe in
-//         our API and our film suggestoin generator which allows you to select the
-//         mood you are in and then be suggested a movie.
-//       </p>
-//       {/* <p>FilmFeels is a REST API
-//         that groups movies by mood tags. Users can submit a film and mood via
-//         the front end, which our backend enriches with extra data. The API
-//         powers a movie suggestion generator that recommends films based on your
-//         mood.</p> */}
-//     </div>
-//   )
-// }
-
-// export default FilmFeels
 function FilmFeels() {
   return (
     <div
