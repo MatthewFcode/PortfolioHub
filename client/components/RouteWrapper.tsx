7d4e20f0d6f88656996fc1106c1c06c1fa87b1fe
Matthew Foley <matthewfoley333@gmail.com>
2025-12-09 10:14:09 +1300

    make the switch component appear only on route change but not first mount

--- client/components/RouteWrapper.tsx
+++ client/components/RouteWrapper.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useLocation } from 'react-router'
 import LoadingScreen from './Switch.tsx'
 
@@ -10,9 +10,33 @@ export default function RouteWrapper({ children }: Props) {
   const location = useLocation()
   const [loading, setLoading] = useState(false)
 
+  const firstRender = useRef(true)
+  const prevPath = useRef(location.pathname)
+
   // Trigger loading screen on route change
+  // useEffect(() => {
+  //   setLoading(true)
+  // }, [location.pathname])
   useEffect(() => {
-    setLoading(true)
+    // Skip loading screen on the first page load
+    if (firstRender.current) {
+      firstRender.current = false
+      prevPath.current = location.pathname
+      return
+    }
+
+    const from = prevPath.current
+    const to = location.pathname
+
+    const isRootToInfo =
+      (from === '/' && to === '/info') || (from === '/info' && to === '/')
+
+    if (isRootToInfo) {
+      setLoading(true)
+    }
+
+    // update old path
+    prevPath.current = to
   }, [location.pathname])
 
   return (
