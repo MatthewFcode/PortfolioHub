caaf1a71f2c0a4a083a484d094acfaaad0548ef7
Matthew Foley <matthewfoley333@gmail.com>
2025-12-09 14:32:15 +1300

    add the looped videos to the info page

--- client/components/InfoContent.tsx
+++ client/components/InfoContent.tsx
@@ -1,125 +1,3 @@
-// function Content() {
-//   return (
-//     <>
-//       <div>
-//         <a href="https://github.com/MatthewFcode/InsightStack">GitHub</a>
-//         <a href="https://insightstack.borb.nz/">Deployed Site</a>
-
-//         <img src="/src/assets/images/placeholder.png" alt="InsightStack-demo" />
-//         <h6>TechStack Used:</h6>
-//         <ul>
-//           <li>TypeScript</li>
-//           <li>React.js</li>
-//           <li>Node.js</li>
-//           <li>Express.js</li>
-//           <li>SQLite3</li>
-//           <li>Knex.js</li>
-//           <li>Websockets</li>
-//           <li>Multer</li>
-//           <li>SCSS</li>
-//           <li>Auth0</li>
-//         </ul>
-//         <p>
-//           InsightStack is a posting platform for users to sign in and share
-//           their technical and human skills insights to others. I started
-//           building this before leaving Dev Academy because I wanted to give
-//           people a place to share what they had learned recently that was really
-//           valuable to them.
-//         </p>
-//       </div>
-//       <div>
-//         <a href="https://github.com/MatthewFcode/Echo">GitHub</a>
-//         <a href="https://echo.borb.nz/">Deployed Site</a>
-//         <img src="/src/assets/images/placeholder.png" alt="Echo-demo" />
-//         <h6>TechStack Used:</h6>
-//         <ul>
-//           <li>TypeScript</li>
-//           <li>JavaScript</li>
-//           <li>React.js</li>
-//           <li>Node.js</li>
-//           <li>Express.js</li>
-//           <li>SQLite3</li>
-//           <li>Knex.js</li>
-//           <li>Websockets</li>
-//           <li>Tailwind CSS</li>
-//           <li>Cloudinary</li>
-//           <li>SCSS</li>
-//           <li>Auth0</li>
-//         </ul>
-//         <p>
-//           Echo is a messaging platform that lets you add friends and have a real
-//           time conversation with someone. It has manny different feautres
-//           including a built in typing test and news headline banner. The main
-//           challenge in this project was securing chat ids so that users cannot
-//           snoop into chats that they are not apart of.
-//         </p>
-//       </div>
-
-//       <div>
-//         <a href="https://github.com/MatthewFcode/FilmFeel-API">GitHub</a>
-//         <a href="https://filmfeels.borb.nz/">Deployed Site</a>
-//         <img src="/src/assets/images/placeholder.png" alt="FilmFeels-demo" />
-//         <h6>TechStack Used:</h6>
-//         <ul>
-//           <li>TypeScript</li>
-//           <li>JavaScript</li>
-//           <li>React.js</li>
-//           <li>Node.js</li>
-//           <li>Express.js</li>
-//           <li>SQLite3</li>
-//           <li>Knex.js</li>
-//           <li>Websockets</li>
-//           <li>SCSS</li>
-//         </ul>
-//         <p>
-//           FilmFeels is an API that accepts both POST and GET requests with no
-//           CORS restrictions. There is a form for contributing movies to the
-//           database and an automatic data enrichment process that makes entering
-//           just the title of a movie a whole valuable dataset. The biggest
-//           challenge I face in this project was actually seeding the database. As
-//           a soulution I loop through a huge list of movies in the seeds calling
-//           the third party api for each one and then insert that object into the
-//           database.{' '}
-//         </p>
-//       </div>
-//       <div>
-//         <a href="https://github.com/MatthewFcode/PL-Match-Model">GitHub</a>
-//         {/* <a href="https://filmfeels.borb.nz/">Deployed Site</a> */}
-//         <img src="/src/assets/images/placeholder.png" alt="FilmFeels-demo" />
-//         <ul>
-//           <li>Python</li>
-//           <li>Machine Learning | Random Forest Classifier</li>
-//           <li>Feature Engineering</li>
-//           <li>Web Scraping</li>
-//           <li>Generative AI</li>
-//           <li>Flask APIs</li>
-//           <li>SQLite3</li>
-//           <li>TypeScript</li>
-//           <li>JavaScript</li>
-//           <li>React.js</li>
-//           <li>Express.js</li>
-//           <li>SCSS</li>
-//         </ul>
-//         <p>
-//           Match Model is a map based app that allows you to browse a map of
-//           Premier League teams and view future predictions for the teams. The
-//           biggest challenge here was accuracy. Whilst the webscrapers I built
-//           were very effective at gathering relevant and valuable data to train
-//           the ML model on it was still hard to acheive wide spread accuracy with
-//           some predictions still not logical. To filter out the outliers I
-//           applied generative AI in the Flask API route to revise the dataset and
-//           make changes where necessary considering all relevent Premier League
-//           information.{' '}
-//         </p>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Content
-
-// import './InfoContent.scss'
-
 function Content() {
   return (
     <div className="info-container">
@@ -140,12 +18,14 @@ function Content() {
               </a>
             </div>
           </div>
-
-          <img
+          <video className="project-image" autoPlay loop muted playsInline>
+            <source src="/videos/insightstack.mp4" type="video/mp4" />
+          </video>
+          {/* <img
             src="/src/assets/images/placeholder.png"
             alt="InsightStack-demo"
             className="project-image"
-          />
+          /> */}
 
           <div className="tech-stack">
             <h3 className="section-title">Tech Stack:</h3>
@@ -239,11 +119,9 @@ function Content() {
             </div>
           </div>
 
-          <img
-            src="/src/assets/images/placeholder.png"
-            alt="FilmFeels-demo"
-            className="project-image"
-          />
+          <video className="project-image" autoPlay loop muted playsInline>
+            <source src="/videos/filmfeels.mp4" type="video/mp4" />
+          </video>
 
           <div className="tech-stack">
             <h3 className="section-title">Tech Stack:</h3>
@@ -286,11 +164,9 @@ function Content() {
             </div>
           </div>
 
-          <img
-            src="/src/assets/images/placeholder.png"
-            alt="PL-Match-Model-demo"
-            className="project-image"
-          />
+          <video className="project-image" autoPlay loop muted playsInline>
+            <source src="/videos/matchmodel.mp4" type="video/mp4" />
+          </video>
 
           <div className="tech-stack">
             <h3 className="section-title">Tech Stack:</h3>
