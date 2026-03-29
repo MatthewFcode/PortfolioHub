07109dbb0de6978b1f317839a1717f2da92463b5
Matthew Foley <matthewfoley333@gmail.com>
2026-03-29 22:18:33 +1300

    add python parsing and data validation via BaseModel from pydatnic

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel # class from pydantic that validates data and parses JSON into python objects 
 
 app = FastAPI()
 
 @app.get("/api/v1/alfredo")
 async def read_root():
-    return {"message": "Hello from PortfolioHub Backend!"}
+    return {"message": "Hello from PortfolioHub Backend!"} # no res like we do in express routes we just use the return statement
 
-@app.post("/api/alfredo")
-async def create_something():
-    return {"message": "POST request received"}
+class AlfredoRequest(BaseModel):
+    prompt: str
+
+@app.post("/api/alfredo") # can read data through the path paramters or the request body
+async def alfredo_post_function(data: AlfredoRequest): # validates that data is in that shape 
+    return {
+        "prompt": data.prompt
+    }
+    
