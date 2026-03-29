2ede1d04acc4a2ae76ed028b89ff2ad871feba21
Matthew Foley <matthewfoley333@gmail.com>
2026-03-29 22:00:10 +1300

    chagne the target proxy for forwarding requests in the vite config to the ASGI uvicorn server

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // proxy for modifying traffic between a server and a frontend/client otherwise CORS would block our request from our dev server to our backend server
     api: {
       // sets up the proxy for any request that starts with api
-      target: 'https://localhost:8000',
+      target: 'http://localhost:8000',
       changeOrigin: true,
       rewrite: (path) => path.replace(/^\/api/, ''), // rewriting the request path before sending it to the server e.g. React makes POST to /api/users --> proxy intercepts and reroutes to http://localhost:8000/api/users
     },
