e034934e96dcc37dcdd63d4bee3120d8ed5e4252
Matthew Foley <matthewfoley333@gmail.com>
2026-03-30 09:53:01 +1300

    add the start of the langchain function

--- backend/alfredo/alfredo.py
+++ backend/alfredo/alfredo.py
@@ -1,1 +1,17 @@
 import asyncio
+import os # built in python module that lets us talk to the operating system
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from sentence_transformers import SentenceTransformer
+
+load_dotenv() # loads all the current env vars into the os environ
+
+emberd_model = SentenceTransformer("BAAI/bge-base-en-v1.5")
+
+model = ChatGoogleGeneratvieAI(
+  model="flash-latest", 
+  temperature=0.7,
+  api_key=os.environ["GOOGLE_API_KEY"]
+)
+
